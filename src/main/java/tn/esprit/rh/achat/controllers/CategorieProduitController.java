4677a0f7c3a55a325661088a1373934ad969cf26
wahidzaiem <wahid.zaiem@sesame.com.tn>
2026-04-25 22:59:03 +0100

    Fix

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -3,7 +3,9 @@ package tn.esprit.rh.achat.controllers;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.rh.achat.dto.CategorieProduitDTO;
 import tn.esprit.rh.achat.entities.CategorieProduit;
+import tn.esprit.rh.achat.mapper.CategorieProduitMapper;
 import tn.esprit.rh.achat.services.ICategorieProduitService;
 
 import java.util.List;
@@ -13,45 +15,53 @@ import java.util.List;
 @RequestMapping("/categorieProduit")
 public class CategorieProduitController {
 
-	@Autowired
-	ICategorieProduitService categorieProduitService;
-	
-	// http://localhost:8089/SpringMVC/categorieProduit/retrieve-all-categorieProduit
-	@GetMapping("/retrieve-all-categorieProduit")
-	@ResponseBody
-	public List<CategorieProduit> getCategorieProduit() {
-		List<CategorieProduit> list = categorieProduitService.retrieveAllCategorieProduits();
-		return list;
-	}
+    @Autowired
+    ICategorieProduitService categorieProduitService;
+    
+    // http://localhost:8089/SpringMVC/categorieProduit/retrieve-all-categorieProduit
+    @GetMapping("/retrieve-all-categorieProduit")
+    @ResponseBody
+    public List<CategorieProduitDTO> getCategorieProduit() {
+        List<CategorieProduit> list = categorieProduitService.retrieveAllCategorieProduits();
+        // Conversion Entity → DTO
+        return CategorieProduitMapper.toDTOList(list);
+    }
 
-	// http://localhost:8089/SpringMVC/categorieProduit/retrieve-categorieProduit/8
-	@GetMapping("/retrieve-categorieProduit/{categorieProduit-id}")
-	@ResponseBody
-	public CategorieProduit retrieveCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
-		return categorieProduitService.retrieveCategorieProduit(categorieProduitId);
-	}
+    // http://localhost:8089/SpringMVC/categorieProduit/retrieve-categorieProduit/8
+    @GetMapping("/retrieve-categorieProduit/{categorieProduit-id}")
+    @ResponseBody
+    public CategorieProduitDTO retrieveCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
+        CategorieProduit entity = categorieProduitService.retrieveCategorieProduit(categorieProduitId);
+        // Conversion Entity → DTO
+        return CategorieProduitMapper.toDTO(entity);
+    }
 
-	// http://localhost:8089/SpringMVC/categorieProduit/add-categorieProduit
-	@PostMapping("/add-categorieProduit")
-	@ResponseBody
-	public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
-		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cp);
-		return categorieProduit;
-	}
+    // http://localhost:8089/SpringMVC/categorieProduit/add-categorieProduit
+    @PostMapping("/add-categorieProduit")
+    @ResponseBody
+    public CategorieProduitDTO addCategorieProduit(@RequestBody CategorieProduitDTO cpDTO) {
+        // Conversion DTO → Entity pour le traitement
+        CategorieProduit entity = CategorieProduitMapper.toEntity(cpDTO);
+        CategorieProduit savedEntity = categorieProduitService.addCategorieProduit(entity);
+        // Conversion Entity → DTO pour la réponse
+        return CategorieProduitMapper.toDTO(savedEntity);
+    }
 
-	// http://localhost:8089/SpringMVC/categorieProduit/remove-categorieProduit/{categorieProduit-id}
-	@DeleteMapping("/remove-categorieProduit/{categorieProduit-id}")
-	@ResponseBody
-	public void removeCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
-		categorieProduitService.deleteCategorieProduit(categorieProduitId);
-	}
+    // http://localhost:8089/SpringMVC/categorieProduit/remove-categorieProduit/{categorieProduit-id}
+    @DeleteMapping("/remove-categorieProduit/{categorieProduit-id}")
+    @ResponseBody
+    public void removeCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
+        categorieProduitService.deleteCategorieProduit(categorieProduitId);
+    }
 
-	// http://localhost:8089/SpringMVC/categorieProduit/modify-categorieProduit
-	@PutMapping("/modify-categorieProduit")
-	@ResponseBody
-	public CategorieProduit modifyCategorieProduit(@RequestBody CategorieProduit categorieProduit) {
-		return categorieProduitService.updateCategorieProduit(categorieProduit);
-	}
-
-	
+    // http://localhost:8089/SpringMVC/categorieProduit/modify-categorieProduit
+    @PutMapping("/modify-categorieProduit")
+    @ResponseBody
+    public CategorieProduitDTO modifyCategorieProduit(@RequestBody CategorieProduitDTO categorieProduitDTO) {
+        // Conversion DTO → Entity pour le traitement
+        CategorieProduit entity = CategorieProduitMapper.toEntity(categorieProduitDTO);
+        CategorieProduit updatedEntity = categorieProduitService.updateCategorieProduit(entity);
+        // Conversion Entity → DTO pour la réponse
+        return CategorieProduitMapper.toDTO(updatedEntity);
+    }
 }

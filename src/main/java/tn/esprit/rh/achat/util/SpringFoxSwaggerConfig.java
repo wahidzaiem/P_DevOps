f70554717431b94b6fa964c373704d9a634cbf99
latifaakh01 <latyfaakhsey@gmail.com>
2026-05-04 13:09:22 +0000

    fix: disable Swagger to fix Springfox NullPointerException

--- src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
+++ src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
@@ -1,33 +1,8 @@
 package tn.esprit.rh.achat.util;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SpringFoxSwaggerConfig {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiEndPointsInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("tn.esprit.rh.achat"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder()
-                .title("Achat Project")
-                .description("DevOps Module API Documentation")
-                .version("1.0.0")
-                .build();
-    }
+    // Swagger disabled for Spring Boot 2.7 compatibility
 }
